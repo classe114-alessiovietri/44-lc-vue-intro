a716a3c0c6ad2eb8b18164be60d01d5ee8ea781c
alessiovietri <info@alessiovietri.it>
2023-12-22 12:27:17 +0100

    Completato LC

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,28 @@
-// Test funzionamento
-console.log('Template HTML CSS JS');
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!',
+            firstName: 'Alessio',
+            lastName: 'Vietri',
+            age: 32,
+            wearsGlasses: true,
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, repellendus?',
+            favPkmn: 'squ',
+            pClasses: 'lightcoral bold bordered'
+        }
+    },
+    methods: {
+        getPClasses() {
+            return 'py-4 bg-success';
+        },
+        sayHi() {
+            alert('Ciao user!');
+        },
+        printTextInConsole() {
+            console.log(this.text);
+            console.log(this.getPClasses());
+        }
+    }
+}).mount('#app');
